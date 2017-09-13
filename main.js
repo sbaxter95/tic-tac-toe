b2ae66b2e7028ccfaa9d89cfdafe04f4c1cd641d
sbaxter95 <sbaxter95@outlook.com>
2017-09-13 17:48:22 +0100

    can win horizontally

--- main.js
+++ main.js
@@ -37,11 +37,13 @@ if (moves === 9) {
 }
 
 function logic(arr) {
-	if (arr[0] === arr[1] && arr[0] === arr[2]) {
+	if (arr[0] !== undefined && arr[0] === arr[1] && arr[0] === arr[2]) {
 		checkWinner(arr[0]);
-	} else if (arr[3] === arr[4] && arr[3] === arr[5]) {
+	} else if (arr[3] !== undefined && arr[3] === arr[4] && arr[3] === arr[5]) {
 		checkWinner(arr[3]);
-	}
+	} else if (arr[6] !== undefined && arr[6] === arr[7] && arr[6] === arr[8]) {
+		checkWinner(arr[6]);
+	} 
 }
 
 function checkWinner(index) {
