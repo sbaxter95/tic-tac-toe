e635036068a93c600ea7a4f90599bd2fb35947a0
sbaxter95 <sbaxter95@outlook.com>
2017-09-13 16:21:53 +0100

    grid and basic setup

---
+++ main.js
@@ -0,0 +1,9 @@
+var playerX = true;
+
+//Alternate between X and O 
+
+//After each player has made a move, check to see if someone has won
+
+//If winner, display winner's message
+
+//Maximum of 9 moves - variable - gets to 9 - tie?
